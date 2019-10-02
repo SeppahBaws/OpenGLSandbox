d363511b9759c1006411f418d7f7264f2c49b606
Seppe Dekeyser <seppe@dva.be>
2019-10-02 02:04:06 +0200

    Camera cleanup

--- OpenGLSandbox/src/Camera.h
+++ OpenGLSandbox/src/Camera.h
@@ -11,20 +11,20 @@ public:
 
 	inline glm::vec3 GetPosition() const { return m_Position; }
 	inline void SetPosition(const glm::vec3& newPos) { m_Position = newPos; CalculateViewMatrix(); }
-	inline glm::vec3 GetRotation() const { return m_Rotation; }
-	inline void SetRotation(const glm::vec3& newRot) { m_Rotation = newRot; CalculateViewMatrix(); }
 
 	inline glm::vec3 GetFront() const { return m_Front; }
 	inline glm::vec3 GetRight() const { return m_Right; }
 	inline glm::vec3 GetUp() const { return m_Up; }
 
 	inline void AddYaw(float amount) { m_Yaw += amount; }
+	inline float GetYaw() const { return m_Yaw; }
 	inline void AddPitch(float amount)
 	{
 		m_Pitch += amount;
 		if (m_Pitch >= 89.9f) m_Pitch = 89.9f;
 		if (m_Pitch <= -89.9f) m_Pitch = -89.9f;
 	}
+	inline float GetPitch() const { return m_Pitch; }
 
 private:
 	void CalculateCameraVectors();
@@ -33,7 +33,6 @@ private:
 
 private:
 	glm::vec3 m_Position;
-	glm::vec3 m_Rotation;
 	
 	glm::mat4 m_ViewMatrix = glm::mat4(1.0f);
 	glm::mat4 m_ProjectionMatrix = glm::mat4(1.0f);
@@ -49,6 +48,6 @@ private:
 	glm::vec3 m_Right = { 0.0f, 0.0f, 0.0f };
 	glm::vec3 m_WorldUp = { 0.0f, 0.0f, 0.0f };
 
-	float m_Yaw = 0.0f;
+	float m_Yaw = -90.0f;
 	float m_Pitch = 0.0f;
 };
